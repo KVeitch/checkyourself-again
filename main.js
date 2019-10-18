ae5cb586024b048f808851653331600e70f9ecfd
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-18 00:23:32 -0600

    Add todo list population to the DOM

--- main.js
+++ main.js
@@ -11,20 +11,24 @@ const urgentListBtn = document.querySelector('.js-filter-btn')
 
 //Locations for adding items
 const domTasks = document.querySelector('.js__taskList')
-
+const listContainer = document.querySelector('.main__div-list')
 //Event listeners
 
 document.querySelector('.aside').addEventListener('click', asideClickHandler);
 listItemInput.addEventListener('keydown', inputEnter)
+listContainer.addEventListener('click', mainClickHandeler)
+
+function mainClickHandeler(e) {
+  if(e.target.closest('div').classList.contains('delete-btn')) 
+  e.target.closest('section').remove()
+}
+
 
 function asideClickHandler (e) {
-
   if(e.target.classList.contains('add-task')) addTaskToNav();
-
   if(e.target.classList.contains('js-li-delete')) removeTask(e);
-
   if(e.target.classList.contains('js-make-list-btn')){
-    console.log('here')
+    createList()
   }
 }
 
@@ -56,3 +60,34 @@ const makeNavTask = (taskId) => {
 }
 
 
+const createList= () => {
+  const domTaskList = []
+  
+  document.querySelectorAll('.dom-task').forEach(task => domTaskList.push({id:task.id, text:task.innerText}))
+  const bob = domTaskList.map(domTask => `
+  <li class="task-item" id=${domTask.id}><input class="icon talk-checkbox" type="image" src='./images/checkbox.svg'><p class="task-text">${domTask.text}</p></li>
+  `)
+  console.log('bob: ',bob)
+  listContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<section class="checklist">
+    <div class="tasklist">
+      <div class="list-header">Title</div> 
+      <ul class="task-list">
+        ${bob.join('')}
+        
+      </ul>
+      <footer class="list-footer">
+        <div class="urgent-btn">
+          <img class='icon' src='./images/urgent.svg' />
+          urgent
+        </div>
+        <div class="delete-btn">
+          <img class='icon' src='./images/delete.svg' />
+          All done
+        </div>
+      </footer>
+    </div>
+  </section>`
+  );
+};
